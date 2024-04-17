6b099935eeb017fd682f3bc526edf2df92cf8040
Shane Kory <167286183+bottledipod@users.noreply.github.com>
2024-04-17 09:49:43 +0300

    Create text.cpp

---
+++ text.cpp
@@ -0,0 +1,33 @@
+bool GetInt(string::const_iterator& s_it,
+  const string::const_iterator& it_end, int64_t& value)
+{
+  value = 0;
+  while (s_it != it_end && *s_it == ' ') ++s_it;
+  if (s_it == it_end) return false;
+  bool is_neg = (*s_it == '-');
+  if (is_neg) ++s_it;
+  string::const_iterator s_it2 = s_it;
+  while (s_it != it_end && *s_it >= '0' && *s_it <= '9')
+  {
+    value = value * 10 + static_cast<int64_t>(*s_it++) - 48;
+  }
+
+  if (s_it == s_it2) return false; //no value
+  //trim trailing space and a comma if present
+  while (s_it != it_end && *s_it == ' ') ++s_it;
+  if (s_it != it_end && *s_it == ',') ++s_it;
+  if (is_neg) value = -value;
+  return true;
+}
+
+bool GetPath(const string& line, Paths64& paths)
+{
+  Path64 p;
+  int64_t x = 0, y = 0;
+  string::const_iterator s_it = line.cbegin(), s_end = line.cend();
+  while (GetInt(s_it, s_end, x) && GetInt(s_it, s_end, y))
+    p.push_back(Point64(x, y));
+  if (p.empty()) return false;
+  paths.push_back(p);
+  return true;
+}
